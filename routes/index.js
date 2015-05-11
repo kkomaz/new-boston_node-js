112a141acc5bf8e50806726072218f37ea8b6346
kkomaz <leeajl006@gmail.com>
2015-05-11 14:02:11 -0400

    first commit ejs config

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+//Controller index
+
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {
+    title: 'Express',
+    name: 'Bucky'
+  });
+});
+
+module.exports = router;
