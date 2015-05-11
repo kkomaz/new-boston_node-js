449d8b0890dd38ee1e80fb58ef57d07c3218809c
kkomaz <leeajl006@gmail.com>
2015-05-11 14:36:01 -0400

    add json data

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ var about = require('./routes/about');
 
 var app = express();
 
+app.locals.points = "8,700";
+app.locals.videodata = require('./videodata.json');
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
